bb4f76d73d839524bf2c201a288bfa41d3deb891
Kalel L. Martinho <kalel@tutamail.com>
2024-02-06 15:18:18 -0300

    adicionada excecoes personalizadas

---
+++ rinha_backend_2024_q1_fastapi/exceptions.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException, status
+from enum import Enum
+
+
+class MensagemErro(str, Enum):
+    CLIENTE_NAO_ENCONTRADO = "Cliente não encontrado"
+    SALDO_INSUFICIENTE = "A transação não pode ser realizada por saldo insuficiente"
+
+
+class BaseException(HTTPException):
+    STATUS_CODE = status.HTTP_400_BAD_REQUEST
+    DETAIL = "Erro genérico"
+
+    def __init__(self, **kwargs):
+        super().__init__(status_code=self.STATUS_CODE, detail=self.DETAIL, **kwargs)
+
+
+class SaldoInsuficienteException(BaseException):
+    STATUS_CODE = status.HTTP_422_UNPROCESSABLE_ENTITY
+    DETAIL = MensagemErro.SALDO_INSUFICIENTE
+
+
+class ClienteNaoEncontradoException(BaseException):
+    STATUS_CODE = status.HTTP_404_NOT_FOUND
+    DETAIL = MensagemErro.CLIENTE_NAO_ENCONTRADO
