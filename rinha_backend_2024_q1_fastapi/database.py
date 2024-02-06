5c88ff63f56a6e46d9e4fd7fbb8fa282e287294d
Kalel L. Martinho <kalel@tutamail.com>
2024-02-06 15:18:00 -0300

    adicionado configuracao para database contendo mock para simulacao

---
+++ rinha_backend_2024_q1_fastapi/database.py
@@ -0,0 +1,39 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncEngine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from .config import settings
+
+from .models import Cliente
+
+engine: AsyncEngine = create_async_engine(settings.DB_URL)
+
+
+async def iniciar_db() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+    async with AsyncSession(engine) as session:
+        async with session.begin():
+            clientes = [
+                Cliente(id=1, limite=100000, saldo=0),
+                Cliente(id=2, limite=80000, saldo=0),
+                Cliente(id=3, limite=1000000, saldo=0),
+                Cliente(id=4, limite=10000000, saldo=0),
+                Cliente(id=5, limite=500000, saldo=0),
+            ]
+            session.add_all(clientes)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async_session = async_sessionmaker(
+        engine, expire_on_commit=False, class_=AsyncSession
+    )
+    async with async_session() as session:
+        try:
+            yield session
+        except:
+            await session.rollback()
+        finally:
+            await session.close()
