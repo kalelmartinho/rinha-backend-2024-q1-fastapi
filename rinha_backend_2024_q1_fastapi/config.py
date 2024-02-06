58a6f1d81b0deb6f96ee51b4e22196f4a49290ca
Kalel L. Martinho <kalel@tutamail.com>
2024-02-06 15:17:46 -0300

    adicionado config para database

---
+++ rinha_backend_2024_q1_fastapi/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    DB_DRIVER: str = "postgresql+asyncpg"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "postgres"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "postgres"
+    DB_URL: str = (
+        f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+
+settings = Config()
