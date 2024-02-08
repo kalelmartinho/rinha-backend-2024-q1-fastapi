9859a51aa379648d9f43b9f88ea48a4d20d972c1
Kalel L. Martinho <kalel@tutamail.com>
2024-02-08 13:49:45 -0300

    remove uvicorn function from main.py

--- rinha_backend_2024_q1_fastapi/main.py
+++ rinha_backend_2024_q1_fastapi/main.py
@@ -14,11 +14,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
-
-
-def start_server():
-    import uvicorn
-
-    uvicorn.run(
-        "rinha_backend_2024_q1_fastapi.main:app", host="0.0.0.0", port=8000, reload=True
-    )
